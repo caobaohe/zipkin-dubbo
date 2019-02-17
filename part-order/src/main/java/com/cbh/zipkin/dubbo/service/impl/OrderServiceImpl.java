baa946b998eb2f96dc62700c1644cdc7ed7bc7e7
caobaohe <caobaohe@qq.com>
2019-02-17 15:51:28 +0800

    tracing

--- part-order/src/main/java/com/cbh/zipkin/dubbo/service/impl/OrderServiceImpl.java
+++ part-order/src/main/java/com/cbh/zipkin/dubbo/service/impl/OrderServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-@Service
+@Service(filter = "tracing")
 @Component
 public class OrderServiceImpl implements OrderService {
 
